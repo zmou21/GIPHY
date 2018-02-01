1a2a7bc8e3c9dac681fe447b469de885494fd1c2
Zack <zmoumen13@gmail.com>
2018-01-31 21:31:50 -0700

    added functionality to form field

--- assets/javascript/cargifs.js
+++ assets/javascript/cargifs.js
@@ -6,6 +6,8 @@
 
 $(document).ready(function() {
 
+
+
 	var carArr = ["Chevelle", "Lamborghini", "Bugatti"];
 
 	displayButtons();
@@ -20,7 +22,7 @@ $(document).ready(function() {
 
 	function displayButtons() {
 
-		$("#buttons-view").empty();
+		$(".button-holder").empty();
 
 		for (var i = 0; i < carArr.length; i++) {
 
@@ -34,47 +36,7 @@ $(document).ready(function() {
 		};
 	};
 
-	$("button").on("click", function(event) {
 
-		event.preventDefault();
-
-		var clicked = $(this).attr("data-button");
-
-		// ("button").empty();
-
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + clicked + "&api_key=3WLAsTEKAa7bJ0uW9HpgGcTPhFOwlHmw&limit=10";
-
-		$.ajax({
-
-		url: queryURL,
-		method: "GET"
-		})
-		.then(function(response){
-		
-			console.log(response.data);
-
-			for (var i = 0; i < response.data; i++){
-
-				var gifDiv = $("<div>");
-
-				var image = $("<img src='" + response.data[i].images.fixed_height_small.url + "' alt='car-gifs'>");
-
-				image.attr("data-state", "still");
-
-				image.attr("data-animate-url");
-
-				image.attr("data-still-url");
-
-				var p = $("<p>" + response.data[i].rating.toUpperCase() + "</p>");
-
-
-			}
-
-	 	});
-			
-
-	});
-    
         // When the user clicks one of the buttons - function
             // prevent default
             // get the attribute of the button clicked, and store in a variables
@@ -94,11 +56,66 @@ $(document).ready(function() {
                         // Put the rating from GIPHY response into the paragrapgh created
                         // Append jQuery image to jQuery div
                         // Append jQuery paragrapgh to jQuery div
-                        // Append jQuery div to page
-        // On click of form submit button - function
+                        // Append jQuery div to page	
+
+	$("button").on("click", function(event) {
+
+		event.preventDefault();
+
+		var clicked = $(this).attr("data-button");
+
+		// ("button").empty();
+
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + clicked + "&api_key=3WLAsTEKAa7bJ0uW9HpgGcTPhFOwlHmw&limit=10";
+
+		$.ajax({
+
+		url: queryURL,
+		method: "GET"
+		})
+		.then(function(response){
+
+			console.log(response.data)
+
+			for (var i = 0; i < response.data.length; i++){
+
+				var gifDiv = $("<div>");
+
+				var image = $("<img src='" + response.data[i].images.original_still.url + "' alt='car-gifs'>");
+
+				image.attr("data-state", "still");
+
+				image.attr("data-animate-url");
+
+				image.attr("data-still-url");
+
+				var p = $("<p> Rating: " + response.data[i].rating.toUpperCase() + "</p>");
+
+				gifDiv.prepend(image);
+
+				gifDiv.prepend(p);
+
+				$(".gif-holder").append(gifDiv);
+			};
+
+	 	});		
+
+	});
+
+	        // On click of form submit button - function
             // Create variable of user input text field
             // Push variable just created to array (buttonTitles)
             // Run displayButtons function
+
+	$("#submit").on("click", function() {
+
+		var userInput = $("#user-input").val().trim();
+
+		carArr.push(userInput);
+
+		displayButtons();
+	});
+
         // On click of image div - function
             // Set variable equal to image clicked data-state attribute
             // if (imageState == "still")
